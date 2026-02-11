3432a61f07cc7ab700341347c754ea2fd99c4d25
3OmarReda <omarredaelkholy@gmail.com>
2026-02-11 17:02:04 +0200

    Offer__Module

---
+++ Hotel.Presentation/ViewModels/Offer/AddOfferViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Presentation.ViewModels.Offer
+{
+    public class AddOfferViewModel
+    {
+        public string? Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal DiscountPercentage { get; set; }
+    }
+}
